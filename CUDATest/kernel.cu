5f53c3a56db1321bb83b301fd8c824b93fce9729
TurboGunner <blazinggunner@hotmail.com>
2022-05-09 22:20:36 -0500

    Made a test for OpenVDB containers in kernel.cu.

--- CUDATest/kernel.cu
+++ CUDATest/kernel.cu
@@ -12,6 +12,8 @@
 
 #include <stdio.h>
 
+#include <openvdb/openvdb.h>
+
 using std::vector;
 using std::reference_wrapper;
 using std::function;
@@ -37,5 +39,41 @@ int main()
     cuda_status = cudaDeviceReset();
     CudaExceptionHandler(cuda_status, "cudaDeviceReset failed!");
 
+    OpenVDBTest(simulation);
+
     return 0;
+}
+
+void OpenVDBTest(FluidSim& sim) {
+    VectorField velocity = sim.velocity_;
+
+    openvdb::initialize();
+    openvdb::FloatGrid::Ptr gridX = openvdb::FloatGrid::create(),
+        gridY = openvdb::FloatGrid::create();
+
+    openvdb::FloatGrid::Accessor accessorX = gridX->getAccessor(),
+        accessorY = gridY->getAccessor();
+
+    openvdb::Coord xyz(0, 0, 0);
+
+    unsigned int size = (unsigned int)sqrt(velocity.GetVectorMap().size());
+    unsigned int y_current = 0;
+
+    for (y_current; y_current < size; y_current++) {
+        for (unsigned int i = 0; i < size; i++) {
+            IndexPair current(i, y_current);
+            xyz.reset(i, y_current, 0);
+            accessorX.setValue(xyz, velocity.GetVectorMap()[current].vx);
+            accessorY.setValue(xyz, velocity.GetVectorMap()[current].vy);
+        }
+    }
+
+    gridX->setName("VelocityVectorX");
+    gridY->setName("VelocityVectorY");
+    openvdb::io::File file("VelocityVector.vdb");
+    openvdb::GridPtrVec grids;
+    grids.push_back(gridX);
+    grids.push_back(gridY);
+    file.write(grids);
+    file.close();
 }
